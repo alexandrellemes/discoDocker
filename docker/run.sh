2ec1161e3e057ab387fab1042a0095476711ca8c
Alexandre Pereira LLemes <alexandre.llemes@gmail.com>
2020-08-12 19:45:13 -0300

    Update run.sh
    Exemplo de configuração.

--- docker/run.sh
+++ docker/run.sh
@@ -13,18 +13,18 @@ echo 'Atualiza a maquina virtual'
 #sudo docker-compose exec php-apache apt -y upgrade
 
 echo 'Habilita os sites...'
-sudo docker-compose exec php-apache sh -c "a2ensite pss.local"
+sudo docker-compose exec php-apache sh -c "a2ensite meusite.local"
 
 echo 'Permissoes em pastas...'
 
 # PSS - Sistema Exemplo.
 echo 'PSS - pss/proxy/var/vendor...'
-sudo docker-compose exec php-apache sh -c "chmod 777 /var/www/html/pss/proxy -R"
-sudo docker-compose exec php-apache sh -c "chmod 777 /var/www/html/pss/var -R"
-sudo docker-compose exec php-apache sh -c "chmod 777 /var/www/html/pss/vendor -R"
+sudo docker-compose exec php-apache sh -c "chmod 777 /var/www/html/meusite/proxy -R"
+sudo docker-compose exec php-apache sh -c "chmod 777 /var/www/html/meusite/var -R"
+sudo docker-compose exec php-apache sh -c "chmod 777 /var/www/html/meusite/vendor -R"
 
 #echo 'PSS - composer'
-#sudo docker-compose exec php-apache sh -c "cd /var/www/html/pss && composer install"
+#sudo docker-compose exec php-apache sh -c "cd /var/www/html/meusite && composer install"
 
 echo 'Reinicia o apache2'
 sudo docker-compose exec php-apache sh -c "service apache2 reload"
@@ -36,8 +36,8 @@ echo 'Modulos habilitados...'
 sudo docker-compose exec php-apache php -m | grep -E 'memcache|oci8|uploadprogress|mcrypt'
 
 #echo 'Aguardando o acesso...'
-#sudo docker-compose exec php-apache tail -f /var/log/apache2/pss.local-error.log
-#sudo docker-compose exec php-apache tail -f /var/log/apache2/pss.local-access.log
+#sudo docker-compose exec php-apache tail -f /var/log/apache2/meusite.local-error.log
+#sudo docker-compose exec php-apache tail -f /var/log/apache2/meusite.local-access.log
 
 echo 'Acesso a maquina virtual'
 sudo docker-compose exec php-apache bash
